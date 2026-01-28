1f6a85214dcab30d4a37f563521442f5023684f2
Ayush <ayush.mishra.111213@gmail.com>
2026-01-28 16:08:06 +0530

    reversefun and isPalindrome updated

---
+++ reverse.py
@@ -0,0 +1,7 @@
+'''write a program to find the reverse the given number'''
+num=int(input("Enter a number:"))
+rev=0
+while(num>0):
+    rev = rev*10+num%10
+    num //=10
+print("Reverse num is : ",rev)
